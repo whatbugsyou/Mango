890cf89c16755f66b27ad21f5cc57f779ea555c5
whatbugsyou <1050466416@qq.com>
2023-07-20 17:43:27 +0800

    fix :  config format error
    marshal array error

--- XrayTunnel/PacketTunnelProvider.swift
+++ XrayTunnel/PacketTunnelProvider.swift
@@ -223,12 +223,12 @@ extension MGConfiguration.Model {
             guard let trojan = self.trojan else {
                 throw NSError.newError("\(self.protocolType.description) 构建失败")
             }
-            proxy["settings"] = ["servers": [try JSONSerialization.jsonObject(with: try JSONEncoder().encode(trojan.servers))]]
+            proxy["settings"] = ["servers": try JSONSerialization.jsonObject(with: try JSONEncoder().encode(trojan.servers))]
         case .shadowsocks:
             guard let shadowsocks = self.shadowsocks else {
                 throw NSError.newError("\(self.protocolType.description) 构建失败")
             }
-            proxy["settings"] = ["servers": [try JSONSerialization.jsonObject(with: try JSONEncoder().encode(shadowsocks.servers))]]
+            proxy["settings"] = ["servers": try JSONSerialization.jsonObject(with: try JSONEncoder().encode(shadowsocks.servers))]
         }
         var streamSettings: [String: Any] = [:]
         streamSettings["network"] = self.network.rawValue
